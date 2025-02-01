fd4e9976f6f91d2e9f7f80a2f4877d580d53bfeb
Mohammed-M97 <mohammed-alawlaqi@outlook.com>
2025-02-01 23:48:05 +0300

    add all files

---
+++ index.js
@@ -0,0 +1,8 @@
+function getUsersIds(str){
+    /* let arr = str.split("");
+    let clear = arr.filter((el) => el !== "#" && el !== "/" && el !== "[" && el !== "]" && el !== "'").join("");
+    let reg = 'uid';
+    return clear.replace(reg, "").toLowerCase(); */
+}
+
+console.log(getUsersIds('[\'12345\']'));
